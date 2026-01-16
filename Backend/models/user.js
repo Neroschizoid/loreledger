04d04e71571a12ed26308d77e7992aaa5a4153a6
Rohan Kumar Gautam <justnote0305@gmail.com>
2026-01-16 17:10:09 +0530

    feat: add user domain model

---
+++ Backend/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const userschema = new mongoose.Schema(
+    {
+    name: String,
+    email: String
+    },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User",userschema);
